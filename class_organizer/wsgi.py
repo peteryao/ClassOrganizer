9e24f64f2dc4c1110f5f8a693723857aa4df497b
Peter Yao <peteryao916@gmail.com>
2013-12-07 14:37:52 -0800

    Started Project

---
+++ class_organizer/wsgi.py
@@ -0,0 +1,14 @@
+"""
+WSGI config for class_organizer project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
+"""
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "class_organizer.settings")
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
