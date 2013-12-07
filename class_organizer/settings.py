107984b3769dcad2fa3feca200b3d39f12f295e5
Peter Yao <peteryao916@gmail.com>
2013-12-07 14:42:43 -0800

    Updated setting files.

--- class_organizer/settings.py
+++ class_organizer/settings.py
@@ -10,7 +10,8 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+project = lambda f: os.path.join(PROJECT_ROOT, f) # use as project('db')
 
 
 # Quick-start development settings - unsuitable for production
@@ -58,7 +59,7 @@ WSGI_APPLICATION = 'class_organizer.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': project('db/data.sqlite'),
     }
 }
 
@@ -67,7 +68,7 @@ DATABASES = {
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Los_Angeles'
 
 USE_I18N = True
 
@@ -79,4 +80,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = project('static/')
+
+TEMPLATE_DIRS = (
+  project('templates'),
+)
