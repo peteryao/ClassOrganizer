86f83ac715d0893ef8fa885eb28a6e9646623f3e
Peter Yao <peteryao916@gmail.com>
2013-12-08 10:16:16 -0800

    yay

--- class_organizer/urls.py
+++ class_organizer/urls.py
@@ -12,6 +12,4 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
     url(r'^', include('core.urls')),
     url(r'^admin/', include(admin.site.urls)),
-) 
-
-urlpatterns += staticfiles_urlpatterns()
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # And serve media in dev
